5b9a3f4705161ddfb3168bb359811817d943766a
Damian Mangold <damianmangold@gmail.com>
2024-11-23 11:44:38 -0300

    Add VirtualDriveAdapter class to integrate VirtualDrive with CloudStorage

--- Ch01-Adapter/01_05/end/cloud-storage.cpp
+++ Ch01-Adapter/01_05/end/cloud-storage.cpp
@@ -65,6 +65,27 @@ public:
     const int totalSpace = 15;
 };
 
+class VirtualDriveAdapter : public CloudStorage
+{
+public:
+    VirtualDriveAdapter() : m_virtualDrive(make_unique<VirtualDrive>()) {}
+
+    bool uploadContents(const string &content) override
+    {
+        return m_virtualDrive->uploadData(content, 1);
+    }
+
+    int getFreeSpace() override
+    {
+        const auto& free = m_virtualDrive->totalSpace - m_virtualDrive->usedSpace();
+        cout << "Available VirtualDrive storage: " << free << "GB" << endl;
+        return free;
+    }
+
+private:
+    unique_ptr<VirtualDrive> m_virtualDrive;
+};
+
 int main()
 {
     // Create an array of pointers to CloudStorage objects.
@@ -72,13 +93,14 @@ int main()
     {
         std::make_unique<CloudDrive>(),
         std::make_unique<FastShare>(),
+        std::make_unique<VirtualDriveAdapter>()
     };
 
     // Iterate through the array and invoke the uploadContents and getFreeSpace
     // methods on each object.
     const string content = "Beam me up, Scotty!";
     for (const auto &service : cloudServices)
-    {        
+    {
         service->uploadContents(content);
         service->getFreeSpace();
         cout << endl;
